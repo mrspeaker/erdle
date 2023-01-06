2add2903300424c22644d671ca28138bf7474c0e
Mr Speaker <mrspeaker@gmail.com>
2023-01-06 09:56:44 +0800

    boop

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 
-const letters = "abcdefghijklmnopqrstuvwxyz".split("");
-type letter = typeof letters;
-const no_letter: unique symbol = Symbol("none");
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+type Alpha = typeof alphabet;
+const Unguessed: unique symbol = Symbol("none");
 
-type letter_guess = letter | typeof no_letter;
-type Guess = [
-  letter_guess,
-  letter_guess,
-  letter_guess,
-  letter_guess,
-  letter_guess
-];
+type Guess = Alpha | typeof Unguessed;
+type GuessState = Guess[];
 
-const create_word = (str: string) => str.split("");
-const reset_word = (word: Guess): Guess => word.map(() => no_letter);
+const create_word = (str: string): GuessState => str.split("");
+const reset_word = (word: GuessState): GuessState => word.map(() => Unguessed);
 
 function App() {
   return <div className="App">hi</div>;
