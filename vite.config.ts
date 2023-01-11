e950d76f2ed9318659549a3a840c0ff3cc84090f
Mr Speaker <mrspeaker@gmail.com>
2023-01-11 20:02:15 +0800

    Add build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
-import { defineConfig } from "vite";
-
 // https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import { resolve } from "path";
 export default defineConfig({
   plugins: [],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        create: resolve(__dirname, "create.html"),
+      },
+    },
+  },
 });
